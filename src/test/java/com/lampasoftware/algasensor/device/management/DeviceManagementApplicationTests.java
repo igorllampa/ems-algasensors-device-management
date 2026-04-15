8cb8d58905069e995f5d0110df9ec80b16ad4e0b
igorl <igorllampa@gmail.com>
2026-04-15 09:18:24 +0100

    chore(init): bootstrap Spring Boot project using Spring Initializr

---
+++ src/test/java/com/lampasoftware/algasensor/device/management/DeviceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lampasoftware.algasensor.device.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
