23d62fccad9ac1849d4d2a90bdd9cddaef5de14f
igorl <igorllampa@gmail.com>
2026-04-28 09:20:45 +0100

    feat: add IdGenerator class that is responsible for generating IDs using TSID strategy to avoid ids colision at microservices

---
+++ src/test/java/com/lampasoftware/algasensor/device/management/TSIDTest.java
@@ -0,0 +1,33 @@
+package com.lampasoftware.algasensor.device.management;
+
+import io.hypersistence.tsid.TSID;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+public class TSIDTest {
+
+    @Test
+    public void shouldGenerateTSID(){
+        TSID tsid = TSID.fast();
+        System.out.println(tsid);
+        System.out.println(tsid.toLong());
+        System.out.println(tsid.getInstant());
+
+        TSID tsid2 = TSID.Factory.getTsid();
+        System.setProperty("tsid.node", "2");
+        System.setProperty("tsid.node.count", "32");
+        System.out.println(tsid2);
+        System.out.println(tsid2.toLong());
+        System.out.println(tsid2.getInstant());
+
+        TSID tsid3 = IdGenerator.generateTSID();
+        Assertions.assertThat(tsid3.getInstant())
+                .isCloseTo(Instant.now(), Assertions.within(1, ChronoUnit.MINUTES));
+
+
+
+    }
+}
