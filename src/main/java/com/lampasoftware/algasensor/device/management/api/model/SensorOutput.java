981f6bc02465a6c0dacc5bd4232518b210bfc869
igorl <igorllampa@gmail.com>
2026-05-11 21:10:12 +0100

    feat: add controller and dto for input/output sensors

---
+++ src/main/java/com/lampasoftware/algasensor/device/management/api/model/SensorOutput.java
@@ -0,0 +1,19 @@
+package com.lampasoftware.algasensor.device.management.api.model;
+
+import com.lampasoftware.algasensor.device.management.domain.model.SensorId;
+import io.hypersistence.tsid.TSID;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class SensorOutput {
+
+    private TSID id;
+    private String name;
+    private String ip;
+    private String location;
+    private String protocol;
+    private String model;
+    private Boolean enabled;
+}
