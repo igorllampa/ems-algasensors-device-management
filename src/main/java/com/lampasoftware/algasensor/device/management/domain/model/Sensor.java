22d64a64d16c6c870e7510f7d5ba71c477f8ec55
igorl <igorllampa@gmail.com>
2026-08-03 11:09:44 +0100

    style: remove unused imports and add necessary ones

--- src/main/java/com/lampasoftware/algasensor/device/management/domain/model/Sensor.java
+++ src/main/java/com/lampasoftware/algasensor/device/management/domain/model/Sensor.java
@@ -1,6 +1,5 @@
 package com.lampasoftware.algasensor.device.management.domain.model;
 
-import io.hypersistence.tsid.TSID;
 import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
