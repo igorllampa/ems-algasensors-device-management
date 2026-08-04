55bfeb73e2e1c41aef3be31a362c8a573a5db022
igorl <igorllampa@gmail.com>
2026-08-04 22:59:00 +0100

    feat: add client responsible to consume temperature monitoring endpoints

---
+++ src/main/java/com/lampasoftware/algasensor/device/management/api/client/impl/SensorMonitoringClientImpl.java
@@ -0,0 +1,34 @@
+package com.lampasoftware.algasensor.device.management.api.client.impl;
+
+import com.lampasoftware.algasensor.device.management.api.client.SensorMonitoringClient;
+import io.hypersistence.tsid.TSID;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
+public class SensorMonitoringClientImpl implements SensorMonitoringClient {
+
+    private final RestClient restClient;
+
+    public SensorMonitoringClientImpl(RestClient.Builder restClientBuilder) {
+        this.restClient = restClientBuilder.baseUrl("http://localhost:8082").build();
+    }
+
+    @Override
+    public void enableMonitoring(TSID sensorId) {
+        restClient
+                .put()
+                .uri("/api/sensors/{sensorId}/monitoring/enable", sensorId)
+                .retrieve()
+                .toBodilessEntity();
+    }
+
+    @Override
+    public void disableMonitoring(TSID sensorId) {
+        restClient
+                .delete()
+                .uri("/api/sensors/{sensorId}/monitoring/disable", sensorId)
+                .retrieve()
+                .toBodilessEntity();
+    }
+}
