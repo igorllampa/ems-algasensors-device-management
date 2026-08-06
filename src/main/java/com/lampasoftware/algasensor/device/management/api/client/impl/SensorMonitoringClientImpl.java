a8985f4b7d50460790343ecf41b5fb49a7d214e9
igorl <igorllampa@gmail.com>
2026-08-06 08:52:45 +0100

    feat: configure connection timeout for client requests

--- src/main/java/com/lampasoftware/algasensor/device/management/api/client/impl/SensorMonitoringClientImpl.java
+++ src/main/java/com/lampasoftware/algasensor/device/management/api/client/impl/SensorMonitoringClientImpl.java
@@ -4,8 +4,12 @@ import com.lampasoftware.algasensor.device.management.api.client.SensorMonitorin
 import com.lampasoftware.algasensor.device.management.api.client.SensorMonitoringClientBadGatewayException;
 import io.hypersistence.tsid.TSID;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Component
 public class SensorMonitoringClientImpl implements SensorMonitoringClient {
@@ -15,10 +19,20 @@ public class SensorMonitoringClientImpl implements SensorMonitoringClient {
     public SensorMonitoringClientImpl(RestClient.Builder restClientBuilder) {
         this.restClient = restClientBuilder
                 .baseUrl("http://localhost:8082")
+                .requestFactory(generateClientHttpRequestFactory())
                 .defaultStatusHandler(HttpStatusCode::isError, (request, response) -> {
                     throw new SensorMonitoringClientBadGatewayException();
                 })
                 .build();
+    }
+
+    private ClientHttpRequestFactory generateClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+
+        factory.setReadTimeout(Duration.ofSeconds(5));
+        factory.setConnectTimeout(Duration.ofSeconds(3));
+
+        return factory;
     }
 
     @Override
