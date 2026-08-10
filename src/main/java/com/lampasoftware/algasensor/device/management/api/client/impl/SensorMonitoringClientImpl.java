17e8892a12263ead5a4bd55b34715212ab1d91bc
igorl <igorllampa@gmail.com>
2026-08-10 08:50:38 +0100

    refactor: reduce coupling by using the Factory Pattern for RestClient configuration

--- src/main/java/com/lampasoftware/algasensor/device/management/api/client/impl/SensorMonitoringClientImpl.java
+++ src/main/java/com/lampasoftware/algasensor/device/management/api/client/impl/SensorMonitoringClientImpl.java
@@ -1,38 +1,18 @@
 package com.lampasoftware.algasensor.device.management.api.client.impl;
 
+import com.lampasoftware.algasensor.device.management.api.client.RestClientFactory;
 import com.lampasoftware.algasensor.device.management.api.client.SensorMonitoringClient;
-import com.lampasoftware.algasensor.device.management.api.client.SensorMonitoringClientBadGatewayException;
 import io.hypersistence.tsid.TSID;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
-
-import java.time.Duration;
 
 @Component
 public class SensorMonitoringClientImpl implements SensorMonitoringClient {
 
     private final RestClient restClient;
 
-    public SensorMonitoringClientImpl(RestClient.Builder restClientBuilder) {
-        this.restClient = restClientBuilder
-                .baseUrl("http://localhost:8082")
-                .requestFactory(generateClientHttpRequestFactory())
-                .defaultStatusHandler(HttpStatusCode::isError, (request, response) -> {
-                    throw new SensorMonitoringClientBadGatewayException();
-                })
-                .build();
-    }
-
-    private ClientHttpRequestFactory generateClientHttpRequestFactory() {
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-
-        factory.setReadTimeout(Duration.ofSeconds(5));
-        factory.setConnectTimeout(Duration.ofSeconds(3));
-
-        return factory;
+    public SensorMonitoringClientImpl(RestClientFactory restClientFactory) {
+        this.restClient = restClientFactory.temperatureMonitoringRestClient();
     }
 
     @Override
